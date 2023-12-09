d363807d24b208fc023122bf35e47140d6a46b26
MesachVenegas <mesach.venegas@hotmail.com>
2023-12-09 13:08:37 -0800

    create: footer

---
+++ components/dashboard/footer/Footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className='flex justify-between items-center w-full p-6 bg-slate-200 rounded-lg font-semibold mt-8'>
+      <div>Paqueteria 5 Estrellas</div>
+      <div>© Todo los Derechos Reservados</div>
+    </footer>
+  )
+}
