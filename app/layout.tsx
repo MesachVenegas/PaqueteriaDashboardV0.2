8c44d2d1b9d6c9f13056c6eb4d6ea89790e751ac
MesachVenegas <mesach.venegas@hotmail.com>
2023-12-09 13:10:43 -0800

    add animation to trnasitions

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
-      <body className={`${inter.className}`}>
+      <body className={`${inter.className} transition-all duration-300 ease-in-out`}>
         {children}
       </body>
     </html>
