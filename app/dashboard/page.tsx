ddbb203cb6ebf40d321538874dfd8dca1c55f65e
MesachVenegas <mesach.venegas@hotmail.com>
2023-12-09 13:10:02 -0800

    add: content dashboar main page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,27 @@
+import { Suspense } from "react";
+import Cards from "@/components/dashboard/cards/Cards";
+import Chart from "@/components/dashboard/chart/Chart";
+import Rightbar from "@/components/dashboard/rigthbar/Rightbar";
+import Transactions from "@/components/dashboard/transactions/Transactions";
 
 export default function DashboardPage() {
   return (
-    <div>DashboardPage</div>
+    <div className="grid grid-cols-4 p-5 gap-5 mt-5">
+      <div className="col-span-full xl:col-span-3 flex flex-col gap-5">
+        <div className="flex w-full gap-5">
+            <Cards title="Clientes totales" type="clients" counter={1200} profit={12}/>
+            <Cards title="Ventas del Dia" type="sales" symbol="$" counter={12200} profit={7}/>
+            <Cards title="Ventas Mensuales" type="monthlySales" symbol="$" counter={120000} profit={15}/>
+        </div>
+        <Suspense>
+          <Transactions />
+          <Chart />
+        </Suspense>
+      </div>
+
+      <div className="hidden xl:flex xl:col-span-1">
+        <Rightbar />
+      </div>
+    </div>
   )
 }
