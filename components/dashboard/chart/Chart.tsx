5349e886e923bf85b9d31074d290c5b55da80635
MesachVenegas <mesach.venegas@hotmail.com>
2023-12-09 13:08:55 -0800

    create: chart of clients per month

---
+++ components/dashboard/chart/Chart.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { dataChart } from '@/app/libs/utils';
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+
+export default function Chart() {
+  return (
+    <div className='flex flex-col gap-5 w-full h-[600px] p-5 bg-slate-200 dark:bg-slate-950 rounded-lg'>
+      <h2 className='font-medium text-gray-500 dark:text-gray-300'>Clientes por mes</h2>
+      <ResponsiveContainer width="100%" height="100%">
+        <AreaChart
+          width={500}
+          height={400}
+          data={dataChart}
+          margin={{
+            top: 10,
+            right: 30,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Area type="bump" dataKey="clientes" stroke="#5D97F5" fill="#5D97F5" />
+        </AreaChart>
+      </ResponsiveContainer>
+    </div>
+  )
+}
