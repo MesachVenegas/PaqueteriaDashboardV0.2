9b2805477a094fbc2ce65b2d7d1e226a9bc2367e
MesachVenegas <mesach.venegas@hotmail.com>
2023-12-09 13:10:24 -0800

    add footer to layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/dashboard/footer/Footer";
 import Navbar from "@/components/dashboard/navbar/Navbar";
 import Sidebar from "@/components/dashboard/sidebar/Sidebar";
 
@@ -7,9 +8,10 @@ export default function layout({ children } : {children: React.ReactNode}) {
       <aside className="flex">
         <Sidebar />
       </aside>
-      <main className="flex-1 bg-slate-50 dark:bg-slate-900 dark:text-white p-5">
+      <main className="flex-1 h-screen overflow-y-auto bg-slate-50 dark:bg-slate-900 dark:text-white p-5">
         <Navbar />
         {children}
+        <Footer />
       </main>
     </div>
   )
