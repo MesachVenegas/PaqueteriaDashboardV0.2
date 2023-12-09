45687068b6a7500ed8a12cc0faed9a3f19f9e64f
MesachVenegas <mesach.venegas@hotmail.com>
2023-12-09 13:09:10 -0800

    create: cards data info main

---
+++ components/dashboard/cards/Cards.tsx
@@ -0,0 +1,33 @@
+import { faMoneyBill, faUsers } from '@fortawesome/free-solid-svg-icons'
+import { faCalendar } from '@fortawesome/free-solid-svg-icons/faCalendar'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const icons = {
+  clients: <FontAwesomeIcon icon={faUsers} className='w-5 h-5'/>,
+  sales: <FontAwesomeIcon icon={faMoneyBill} className='w-5 h-5'/>,
+  monthlySales: <FontAwesomeIcon icon={faCalendar} className='w-5 h-5'/>,
+}
+
+export default function Cards({ title, counter, profit, type, symbol } : {
+  title: string,
+  counter: number,
+  profit: number,
+  symbol?: string,
+  type: 'clients' | 'sales' | 'monthlySales'
+}) {
+  const Icon = icons[type]
+
+  return (
+    <div className='flex bg-slate-200 dark:bg-slate-950 hover:bg-slate-300 w-full rounded-lg gap-2 p-5 cursor-pointer'>
+      { Icon ? Icon : null}
+      <div className='flex flex-col gap-5'>
+        <span>{title}</span>
+        <span className='text-2xl font-medium'>{symbol} {counter}</span>
+        <span>
+          <span className={`text-green-600`}>{profit}% </span>
+          mas que la semana pasada
+        </span>
+      </div>
+    </div>
+  )
+}
