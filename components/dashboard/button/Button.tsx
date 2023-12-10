e1f1676aa38ae6eda307a1dad4113dd995c40510
MesachVenegas <mesach.venegas@hotmail.com>
2023-12-10 11:45:23 -0800

    add: types to botton and link source

--- components/dashboard/button/Button.tsx
+++ components/dashboard/button/Button.tsx
@@ -1,22 +1,38 @@
 import { IconDefinition, faUserPlus } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Link from 'next/link'
 import { MouseEventHandler } from 'react'
 
 export default function Button({
   className,
   icon,
   text,
-  action
+  action,
+  type,
+  source
 }:{
   className?: string,
   icon: IconDefinition,
   text: string,
-  action?: MouseEventHandler
+  type: 'link' | 'button',
+  action?: MouseEventHandler,
+  source?: string
 }) {
-  return (
-    <button className={className ? className : 'flex gap-2 items-center py-2 px-4 bg-blue-500 hover:bg-blue-600 rounded-lg text-white'} onClick={action}>
-      <FontAwesomeIcon icon={icon} className='w-5 h-5' />
-      <span className='hidden lg:block'>{text}</span>
-    </button>
-  )
+
+  if(type === 'link'){
+    return (
+      <Link href={source? source : '#'} className={className ? className : 'flex gap-2 items-center py-2 px-4 bg-blue-500 hover:bg-blue-600 rounded-lg text-white'} onClick={action}>
+        <FontAwesomeIcon icon={icon} className='w-5 h-5' />
+        <span className='hidden lg:block'>{text}</span>
+      </Link>
+    )
+  }else {
+    return (
+      <button className={className ? className : 'flex gap-2 items-center py-2 px-4 bg-blue-500 hover:bg-blue-600 rounded-lg text-white'} onClick={action}>
+        <FontAwesomeIcon icon={icon} className='w-5 h-5' />
+        <span className='hidden lg:block'>{text}</span>
+      </button>
+    )
+  }
+
 }
