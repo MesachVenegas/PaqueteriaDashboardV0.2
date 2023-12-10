0268f28d6fe34922abd7dee5943432a379d1356e
MesachVenegas <mesach.venegas@hotmail.com>
2023-12-10 11:46:54 -0800

    create: client create page

---
+++ app/dashboard/clientes/create/page.tsx
@@ -0,0 +1,9 @@
+import FormClient from "@/components/dashboard/clients/form/FormClient";
+
+export default function CreateClient() {
+  return (
+    <div>
+      <FormClient title="Agregar Nuevo Cliente" type="create" />
+    </div>
+  )
+}
