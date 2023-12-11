27955e151a1ffbaa5429d40ad0606827d3adfcaa
MesachVenegas <mesach.venegas@hotmail.com>
2023-12-10 16:13:53 -0800

    add: client filter on search params

--- app/dashboard/clientes/page.tsx
+++ app/dashboard/clientes/page.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from 'react';
 import { Metadata } from 'next'
 import ClientsTable from "@/components/dashboard/tables/ClientsTable";
 import Pagination from "@/components/dashboard/pagination/Pagination";
 import SearchBar from "@/components/dashboard/search/SearchBar";
 import Button from '@/components/dashboard/button/Button';
 import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
+import { getClients } from '@/app/libs/data';
+import { ClientProps } from '@/app/libs/definitions';
 
 
 export const metadata: Metadata = {
@@ -11,8 +14,11 @@ export const metadata: Metadata = {
   description: 'Dashboard App home page',
 }
 
-export default function Clients() {
-
+export default async function Clients({ searchParams }: { searchParams: { search: string, page: string }}) {
+  const search = searchParams?.search || '';
+  const page = searchParams?.page || '1';
+  const response = await getClients(search, parseInt(page));
+  const { count, clients } = Array.isArray(response) ? { count: 0, clients: [] } : response;
 
   return (
     <div className="flex flex-col gap-6 p-5">
@@ -21,8 +27,10 @@ export default function Clients() {
         <Button type='link' source='/dashboard/clientes/create' text="Agregar Cliente" icon={faUserPlus} />
       </div>
       <div className="flex flex-col">
-        <ClientsTable />
-        <Pagination />
+        <Suspense fallback={<span>Cargando...</span>}>
+          <ClientsTable data={clients as ClientProps[]} />
+        </Suspense>
+        <Pagination count={count}/>
       </div>
     </div>
   )
