b1ef4a928113cef638e68a2b542171f163a976aa
MesachVenegas <mesach.venegas@hotmail.com>
2023-12-10 16:11:09 -0800

    add: search function

--- components/dashboard/search/SearchBar.tsx
+++ components/dashboard/search/SearchBar.tsx
@@ -1,8 +1,29 @@
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
+'use client'
+
+import { usePathname, useSearchParams, useRouter } from 'next/navigation';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useDebouncedCallback } from 'use-debounce';
+
 
 export default function SearchBar({ placeholder} : {placeholder: string} ) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const params = new URLSearchParams(searchParams);
+
+
+  const handleSearch = useDebouncedCallback((data: string) => {
+    params.set('page', '1');
+    if(data){
+      data.length > 2 && params.set('search', data);
+    }else {
+      params.delete('search');
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
+
   return (
     <div className='flex max-w-md bg-slate-300/50 dark:bg-slate-600 gap-2 p-2 rounded-lg'>
       <FontAwesomeIcon icon={faSearch} className='w-5 h-5'/>
@@ -10,6 +31,7 @@ export default function SearchBar({ placeholder} : {placeholder: string} ) {
         id="search"
         type="search"
         name="search"
+        onChange={(e) => handleSearch(e.target.value)}
         className='w-full bg-transparent focus:border-none focus:outline-none'
         placeholder={placeholder}
       />
