443f350c0ef34ec1af97ae0d9c9d6edb866ab6d8
MesachVenegas <mesach.venegas@hotmail.com>
2023-12-11 14:27:42 -0800

    changes: load data of fetch

--- components/admin/Tables/UsersTable.tsx
+++ components/admin/Tables/UsersTable.tsx
@@ -1,33 +1,13 @@
+'use client'
+
+import { UserProps } from "@/app/libs/definitions";
 import { faBlackTie } from "@fortawesome/free-brands-svg-icons";
 import { faEdit, faTrash, faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Card, Table, TableBody, TableCell, TableHead, TableHeaderCell, TableRow, Text, Title } from "@tremor/react";
+import { format } from 'date-fns';
 
-const data = [
-  {
-    username: "v.amhered",
-    name: "Viola",
-    last_name: "Amherd",
-    description: "Federal Councillor",
-    is_admin: true,
-  },
-  {
-    username: "s.sommarugaa",
-    name: "Simonetta",
-    last_name: "Amherd",
-    description: "Federal Councillor",
-    is_admin: false
-  },
-  {
-    username: "s.sommarugaa",
-    name: "Simonetta",
-    last_name: "Sommaruga",
-    description: "Federal Councillor",
-    is_admin: false
-  },
-];
-
-export default function UsersTable() {
+export default function UsersTable({ data, removeUser }: { data: UserProps[], removeUser: (id: string) => void }) {
 
   return(
     <Card>
@@ -38,13 +18,11 @@ export default function UsersTable() {
             <TableHeaderCell>Usuario</TableHeaderCell>
             <TableHeaderCell>Nombre</TableHeaderCell>
             <TableHeaderCell>Rol</TableHeaderCell>
-            <TableHeaderCell>Registro</TableHeaderCell>
-            <TableHeaderCell>Actualizado</TableHeaderCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {data.map((item) => (
-            <TableRow key={item.name} className="hover:bg-slate-200">
+            <TableRow key={item.id} className="hover:bg-slate-200">
               <TableCell>
                 <Text>
                   {item.username}
@@ -70,18 +48,12 @@ export default function UsersTable() {
                     )
                   }
               </TableCell>
-              <TableCell>
-                02/12/2023
-              </TableCell>
-              <TableCell>
-                12/12/2023
-              </TableCell>
               <TableCell className="flex gap-4 items-center font-semibold">
                 <span className="flex items-center gap-2 text-green-500 hover:text-green-600 cursor-pointer">
                   <FontAwesomeIcon icon={faEdit} className="w-4 h-4" />
                   Editar
                 </span>
-                <span className="flex items-center gap-2 text-red-600 hover:text-red-500 cursor-pointer">
+                <span className="flex items-center gap-2 text-red-600 hover:text-red-500 cursor-pointer" onClick={() => removeUser(item.id)}>
                   <FontAwesomeIcon icon={faTrash} className="w-4 h-4" />
                   Eliminar
                 </span>
