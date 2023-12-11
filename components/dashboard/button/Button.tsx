8debebc98c8178b1f48a00158b80bc5a724521bf
MesachVenegas <mesach.venegas@hotmail.com>
2023-12-10 23:10:56 -0800

    changes: buttons style props and color handler

--- components/dashboard/button/Button.tsx
+++ components/dashboard/button/Button.tsx
@@ -1,35 +1,38 @@
-import { IconDefinition, faUserPlus } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import Link from 'next/link'
-import { MouseEventHandler } from 'react'
+'use client'
 
-export default function Button({
-  className,
-  icon,
-  text,
-  action,
-  type,
-  source
-}:{
-  className?: string,
-  icon: IconDefinition,
-  text: string,
-  type: 'link' | 'button',
-  action?: MouseEventHandler,
-  source?: string
-}) {
+import Link from 'next/link'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { ButtonProps } from '@/app/libs/definitions'
+
+export default function Button({ icon, text, action, type, source, color }: ButtonProps) {
+
+
 
   if(type === 'link'){
     return (
-      <Link href={source? source : '#'} className={className ? className : 'flex gap-2 items-center py-2 px-4 bg-blue-500 hover:bg-blue-600 rounded-lg text-white'} onClick={action}>
-        <FontAwesomeIcon icon={icon} className='w-5 h-5' />
+      <Link href={source? source : '#'} className={
+          color ? `flex gap-2 items-center py-2 px-4 bg-${color}-500 hover:bg-${color}-600 rounded-lg text-white`
+          :'flex gap-2 items-center py-2 px-4 bg-blue-500 hover:bg-blue-600 rounded-lg text-white'
+        }
+        onClick={action}
+      >
+        {
+          icon ? <FontAwesomeIcon icon={icon} className='w-5 h-5' /> : null
+        }
         <span className='hidden lg:block'>{text}</span>
       </Link>
     )
   }else {
     return (
-      <button className={className ? className : 'flex gap-2 items-center py-2 px-4 bg-blue-500 hover:bg-blue-600 rounded-lg text-white'} onClick={action}>
-        <FontAwesomeIcon icon={icon} className='w-5 h-5' />
+      <button className={
+        color ? `flex gap-2 items-center py-2 px-4 bg-${color}-500 hover:bg-${color}-600 rounded-lg text-white`
+        : 'flex gap-2 items-center py-2 px-4 bg-blue-500 hover:bg-blue-600 rounded-lg text-white'
+        }
+        onClick={action}
+      >
+        {
+          icon ? <FontAwesomeIcon icon={icon} className='w-5 h-5' /> : null
+        }
         <span className='hidden lg:block'>{text}</span>
       </button>
     )
