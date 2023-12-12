2f9d1f796a274425143d835533946fda5c4ca2fc
MesachVenegas <mesach.venegas@hotmail.com>
2023-12-12 10:54:43 -0800

    add: sale page content

--- app/dashboard/ventas/page.tsx
+++ app/dashboard/ventas/page.tsx
@@ -1,3 +1,11 @@
+import { getClients } from '@/app/libs/data';
+import { ClientProps } from '@/app/libs/definitions';
+import Button from '@/components/dashboard/button/Button';
+import Pagination from '@/components/dashboard/pagination/Pagination';
+import ClientSaleTable from '@/components/dashboard/sales/ClientSaleTable'
+import SaleForm from '@/components/dashboard/sales/SaleFomr';
+import SearchBar from '@/components/dashboard/search/SearchBar'
+import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
 import { Metadata } from 'next'
 
 export const metadata: Metadata = {
@@ -5,8 +13,27 @@ export const metadata: Metadata = {
   description: 'Dashboard App home page',
 }
 
-export default function Sales() {
+export default async function Sales({ searchParams }: { searchParams: { search: string, page: string }}) {
+  const search = searchParams?.search || '';
+  const page = searchParams?.page || '1';
+  const response = await getClients(search, parseInt(page));
+  const { count, clients } = Array.isArray(response) ? { count: 0, clients: [] } : response;
+
   return (
-    <div>Sales</div>
+    <div className='flex flex-col gap-4  p-6'>
+      <div className='flex w-full justify-between'>
+        <SearchBar placeholder='Buscar un Cliente' />
+        <Button text='Agregar Cliente' icon={faUserPlus} type='button' />
+      </div>
+      <div className='flex gap-4'>
+        <div className='flex flex-col gap-4 max-w-md w-full'>
+          <ClientSaleTable data={clients as ClientProps[]}/>
+          <Pagination count={count} />
+        </div>
+        <div>
+          <SaleForm />
+        </div>
+      </div>
+    </div>
   )
 }
