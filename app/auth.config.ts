be18d50560abcb4309d42b99bcbac78688f120e6
MesachVenegas <mesach.venegas@hotmail.com>
2023-12-12 10:54:59 -0800

    typo: add comments

--- app/auth.config.ts
+++ app/auth.config.ts
@@ -1,20 +1,27 @@
+import { signIn } from "./auth";
+
 export const authConfig = {
   providers: [],
   pages: {
     signIn: '/login',
   },
   callbacks: {
-    authorized({ auth, request }: { auth: any, request: Request }) {
+    authorized({ auth, request }) {
       const isLoggedIn = auth?.user
       const isOnDashboard = request.nextUrl.pathname.startsWith('/dashboard')
 
+      //? Si el usuario esta en el dashboard se comprueba que este logeado.
       if (isOnDashboard) {
+        //? Si esta logeado se le permite seguir ahi
         if (isLoggedIn) return true;
+        //? Si no se le regresa a la pagina de login
         return false;
-      } else if (isLoggedIn) {
+      }
+      //? Si el usuario se encuentra logeado se le redirecciona al dashboard
+      else if (isLoggedIn) {
         return Response.redirect(new URL('/dashboard', request.nextUrl))
       }
       return true;
-    }
-  }
+    },
+  },
 }
