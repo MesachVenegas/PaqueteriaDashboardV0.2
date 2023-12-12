f1f1c3829e50eab92348939614b6cb3821583dd1
MesachVenegas <mesach.venegas@hotmail.com>
2023-12-12 10:51:57 -0800

    changes: menu options to user and admin roles

--- components/dashboard/sidebar/Sidebar.tsx
+++ components/dashboard/sidebar/Sidebar.tsx
@@ -12,7 +12,6 @@ import { SessionProps } from "@/app/libs/definitions";
 export default function Sidebar({ closeSession, userSignIn } : {closeSession: () => void , userSignIn: SessionProps } ) {
   const pathname = usePathname();
   const { user } = userSignIn;
-  console.log(userSignIn);
 
   return (
     <div className="flex flex-col w-64 justify-start items-start h-full bg-slate-200 dark:bg-slate-950 dark:text-white shadow-xl">
@@ -37,20 +36,40 @@ export default function Sidebar({ closeSession, userSignIn } : {closeSession: ()
             <h4 className="text-sm">{link.title}</h4>
             <ul className="flex flex-col w-full gap-2">
               {
-                link.links.map( (option, index) => (
-                  <li key={index} className="">
-                    <Link
-                      href={option.url}
-                      className={
-                        `flex w-full h-full gap-2 px-3 py-2 rounded-md text-sm font-medium  hover:bg-blue-400/60 dark:hover:bg-yellow-500/90 dark:hover:text-black hover:text-black transition-all duration-300 ease-in-out
-                        ${pathname === option.url ? 'bg-blue-500/80 dark:bg-yellow-500 dark:text-black text-white' : 'text-gray-600 dark:text-gray-200'}`
-                      }
-                    >
-                      <FontAwesomeIcon  icon={option.icon} className="w-5 h-5"/>
-                      {option.title}
-                    </Link>
-                  </li>
-                ))
+                link.links.map( (option, index) =>{
+                  if(option.user !== user.is_admin){
+                    return (
+                        <li key={index}>
+                          <Link
+                            href={option.url}
+                            className={
+                              `flex w-full h-full gap-2 px-3 py-2 rounded-md text-sm font-medium  hover:bg-blue-400/60 dark:hover:bg-yellow-500/90 dark:hover:text-black hover:text-black transition-all duration-300 ease-in-out
+                              ${pathname === option.url ? 'bg-blue-500/80 dark:bg-yellow-500 dark:text-black text-white' : 'text-gray-600 dark:text-gray-200'}`
+                            }
+                          >
+                            <FontAwesomeIcon  icon={option.icon} className="w-5 h-5"/>
+                            {option.title}
+                          </Link>
+                        </li>
+                      )
+                    }else if(option.admin === user.is_admin){
+                      return (
+                        <li key={index}>
+                          <Link
+                            href={option.url}
+                            className={
+                              `flex w-full h-full gap-2 px-3 py-2 rounded-md text-sm font-medium  hover:bg-blue-400/60 dark:hover:bg-yellow-500/90 dark:hover:text-black hover:text-black transition-all duration-300 ease-in-out
+                              ${pathname === option.url ? 'bg-blue-500/80 dark:bg-yellow-500 dark:text-black text-white' : 'text-gray-600 dark:text-gray-200'}`
+                            }
+                          >
+                            <FontAwesomeIcon  icon={option.icon} className="w-5 h-5"/>
+                            {option.title}
+                          </Link>
+                        </li>
+                      )
+                    }
+                  }
+                )
               }
             </ul>
           </div>
