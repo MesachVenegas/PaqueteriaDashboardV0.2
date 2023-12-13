f4d4203346bd6b9aec1cb3bbb3e3531bd80ae65c
MesachVenegas <mesach.venegas@hotmail.com>
2023-12-13 11:21:09 -0800

    add: count

--- app/dashboard/provedores/page.tsx
+++ app/dashboard/provedores/page.tsx
@@ -19,7 +19,7 @@ export default function Providers() {
       </div>
       <div className="flex flex-col">
         <ProvidersTable />
-        <Pagination />
+        <Pagination  count={0}/>
       </div>
     </div>
   )
