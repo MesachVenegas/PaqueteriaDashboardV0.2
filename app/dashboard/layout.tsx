df2d6d6390ebc536940ff59d3ccbbd07a7801a4b
MesachVenegas <mesach.venegas@hotmail.com>
2023-12-13 11:15:40 -0800

    changes: types and dark mode

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -3,6 +3,7 @@ import Footer from "@/components/dashboard/footer/Footer";
 import Navbar from "@/components/dashboard/navbar/Navbar";
 import Sidebar from "@/components/dashboard/sidebar/Sidebar";
 import { auth, signOut } from "../auth";
+import { SessionProps } from "../libs/definitions";
 
 export const metadata: Metadata = {
   title: 'Dashboard | Paqueteria 5 Estrellas',
@@ -12,6 +13,7 @@ export const metadata: Metadata = {
 export default async function layout({ children } : {children: React.ReactNode}) {
 
     const session = await auth()
+    const { user } = session || {};
 
   const closeSession = async () => {
     'use server'
@@ -19,11 +21,11 @@ export default async function layout({ children } : {children: React.ReactNode})
   }
 
   return (
-    <div className="flex w-full min-h-screen dark:bg-slate-900 dark:text-white">
+    <div className="flex w-full min-h-screen dark:bg-slate-800 dark:text-white">
       <aside className="flex">
-        <Sidebar closeSession={closeSession} userSignIn={session} />
+        <Sidebar closeSession={closeSession} userSignIn={ user as SessionProps } />
       </aside>
-      <main className="flex-1 h-screen overflow-y-auto bg-slate-50 dark:bg-slate-900 dark:text-white p-5">
+      <main className="flex-1 h-screen overflow-y-auto bg-slate-50 dark:bg-slate-800 dark:text-white p-5">
         <Navbar />
         {children}
         <Footer />
