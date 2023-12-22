0acd6f17a6b49748fe66f7c86d19fcc1126f379a
MesachVenegas <mesach.venegas@hotmail.com>
2023-12-22 03:19:19 -0800

    change load data of client on DDBB

--- components/dashboard/tables/ClientHistory.tsx
+++ components/dashboard/tables/ClientHistory.tsx
@@ -1,7 +1,8 @@
-import { IconDefinition, faCoins, faCreditCard, faCube, faCubes, faEdit, faEye, faFileArchive, faMoneyBill1Wave, faMoneyBillTransfer, faPlaneDeparture, faTrash, faTruckFast } from "@fortawesome/free-solid-svg-icons";
+import { OrderProps } from "@/app/libs/definitions";
+import { valueFormatter } from "@/app/libs/utils";
+import { IconDefinition, faCoins, faCreditCard, faCube, faCubes, faEdit, faEye, faFileArchive, faMoneyBill1Wave, faMoneyBillTransfer, faPlaneDeparture, faTruckFast } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  Badge,
   Card,
   Table,
   TableBody,
@@ -12,70 +13,27 @@ import {
   Text,
   Title,
 } from "@tremor/react";
+import { format } from "date-fns";
 import Link from "next/link";
 
-const data = [
-  {
-    name: "Viola Amherd",
-    Role: "Federal Councillor",
-    departement: "The Federal Department of Defence, Civil Protection and Sport (DDPS)",
-    type: "plane",
-    payment: 'tarjeta'
-  },
-  {
-    name: "Simonetta Sommaruga",
-    Role: "Federal Councillor",
-    departement:
-      "The Federal Department of the Environment, Transport, Energy and Communications (DETEC)",
-    type: "land",
-    payment: 'efectivo'
-  },
-  {
-    name: "Alain Berset",
-    Role: "Federal Councillor",
-    departement: "The Federal Department of Home Affairs (FDHA)",
-    type: "land",
-    payment: 'tarjeta'
-  },
-  {
-    name: "Ignazio Cassis",
-    Role: "Federal Councillor",
-    departement: "The Federal Department of Foreign Affairs (FDFA)",
-    type: "plane",
-    payment: 'efectivo'
-  },
-  {
-    name: "Karin Keller-Sutter",
-    Role: "Federal Councillor",
-    departement: "The Federal Department of Finance (FDF)",
-    type: "plane",
-    payment: 'transferencia'
-  },
-  {
-    name: "Guy Parmelin",
-    Role: "Federal Councillor",
-    departement: "The Federal Department of Economic Affairs, Education and Research (EAER)",
-    type: "land",
-    payment: 'parcial'
-  },
-  {
-    name: "Elisabeth Baume-Schneider",
-    Role: "Federal Councillor",
-    departement: "The Federal Department of Justice and Police (FDJP)",
-    type: "plane",
-    payment: 'tarjeta'
-  },
-];
 
 const iconPayment: { [key: string]: IconDefinition} = {
-  efectivo: faCoins,
-  tarjeta: faCreditCard,
-  transferencia: faMoneyBillTransfer,
-  parcial: faMoneyBill1Wave,
+  cash: faCoins,
+  card: faCreditCard,
+  transfer: faMoneyBillTransfer,
+  partial: faMoneyBill1Wave,
 }
 
-export default function ClientsHistory() {
+const paymentName : { [key: string]: string} = {
+  cash: 'Efectivo',
+  card: 'Tarjeta',
+  transfer: 'Transferencia',
+  partial: 'Parcial',
+}
 
+export default function ClientsHistory({ orders } : { orders: OrderProps[] }) {
+
+  console.log(orders);
   return(
     <Card>
       <Title>Clientes Registrados</Title>
@@ -85,38 +43,27 @@ export default function ClientsHistory() {
             <TableHeaderCell>Folio</TableHeaderCell>
             <TableHeaderCell>Destinatario</TableHeaderCell>
             <TableHeaderCell>Dirección</TableHeaderCell>
-            <TableHeaderCell>Costo</TableHeaderCell>
+            <TableHeaderCell>Envió</TableHeaderCell>
             <TableHeaderCell>Método de pago</TableHeaderCell>
-            <TableHeaderCell>Tipo</TableHeaderCell>
+            <TableHeaderCell>Costo</TableHeaderCell>
             <TableHeaderCell>Remisión</TableHeaderCell>
             <TableHeaderCell>Fecha</TableHeaderCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {data.map((item) => (
-            <TableRow key={item.name} className="hover:bg-slate-200">
-              <TableCell>{item.name}</TableCell>
+          {orders.map((item) => (
+            <TableRow key={item.id} className="hover:bg-slate-200 dark:hover:bg-slate-800">
+              <TableCell>{item.id.slice(0,13)}</TableCell>
               <TableCell>
-                <Text>{item.Role}</Text>
+                <Text>{item.addressed_name}</Text>
               </TableCell>
               <TableCell>
-                <Text>{`example@example.com`}</Text>
-              </TableCell>
-              <TableCell>
-                <Text>{`$ 1,345.00`}</Text>
-              </TableCell>
-              <TableCell>
-                <Text>
-                  <span className="flex items-center gap-2 capitalize font-semibold">
-                    <FontAwesomeIcon icon={iconPayment[item.payment]} className="w-5 h-5"/>
-                    {item.payment}
-                  </span>
-                </Text>
+                <Text>{`${item.addressed_street}, #${item.addressed_number}, ${item.addressed_colony}, ${item.addressed_city}`}</Text>
               </TableCell>
               <TableCell>
                 <Text className="font-semibold">
                   {
-                    item.type === "plane"
+                    item.type === "air"
                     ? (
                       <span className="flex items-center gap-2 text-blue-500">
                         <FontAwesomeIcon icon={faPlaneDeparture} className="w-5 h-5"/>
@@ -133,13 +80,24 @@ export default function ClientsHistory() {
                 </Text>
               </TableCell>
               <TableCell>
+                <Text>
+                  <span className="flex items-center gap-2 capitalize font-semibold">
+                    <FontAwesomeIcon icon={iconPayment[item.payment_type]} className="w-5 h-5"/>
+                    {paymentName[item.payment_type]}
+                  </span>
+                </Text>
+              </TableCell>
+              <TableCell>
+                <Text>{valueFormatter(item.total)} mxn</Text>
+              </TableCell>
+              <TableCell>
                   <Link href="" className="flex gap-2 items-center bg-slate-600 text-white w-fit py-1 px-3 rounded-md">
                     <FontAwesomeIcon icon={faFileArchive} className="w-4 h-4"/>
                     <span>Remisión</span>
                   </Link>
               </TableCell>
               <TableCell>
-                  04/12/2023
+                  {format(item.registerAt, "dd/MM/yyyy hh:mm b")}
               </TableCell>
             </TableRow>
           ))}
