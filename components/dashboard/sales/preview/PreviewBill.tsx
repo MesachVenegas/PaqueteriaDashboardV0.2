ef81deafabedc199d100d3d3ec3cb7e6f8beb39d
MesachVenegas <mesach.venegas@hotmail.com>
2023-12-22 03:19:52 -0800

    add function to save order data on DDBB

--- components/dashboard/sales/preview/PreviewBill.tsx
+++ components/dashboard/sales/preview/PreviewBill.tsx
@@ -3,14 +3,30 @@ import { FormSaleProps, PreviewProps } from "@/app/libs/definitions";
 import { PDFViewer } from "@react-pdf/renderer";
 import BillTemplate from "../pdfGenerator/BillTemplate";
 import { Suspense } from "react";
+import { saveOrder } from "@/app/libs/actions";
 
-export default function PreviewBill({ data, client, volumetric, folio, subtotal, cancel} : PreviewProps) {
+export default function PreviewBill({ data, client, volumetric, folio, subtotal, confirm, cancel} : PreviewProps) {
 
+  const registerOrder = async () => {
+    const orderData = {
+      addresses: data,
+      client: client,
+      volumetric: volumetric,
+      subtotal: subtotal
+    }
+    console.log(data);
+    try {
+      await saveOrder(orderData);
+      confirm(false);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
 
   return (
-    <div className="fixed top-0 left-0 w-full h-screen flex justify-center items-center bg-slate-200/20 backdrop-blur-sm p-1">
-      <div className="w-full max-w-4xl m-auto bg-slate-200 p-4 rounded-md h-screen">
+    <div className="fixed top-0 left-0 w-full h-screen flex justify-center items-center bg-slate-200/20 dark:bg-slate-700/50 backdrop-blur-sm p-1">
+      <div className="w-full max-w-4xl m-auto bg-slate-200 dark:bg-slate-900 p-4 rounded-md h-screen">
         <Suspense fallback={<span>Cargando ...</span>}>
           <PDFViewer className="flex w-full min-h-[90vh] py-4">
               <BillTemplate  data={data as FormSaleProps} folio={folio} client={client} volumetric={volumetric} subtotal={subtotal} />
@@ -20,7 +36,7 @@ export default function PreviewBill({ data, client, volumetric, folio, subtotal,
           <button className="w-full max-w-xs bg-red-400 hover:bg-red-500 py-2 px-4 rounded-md font-bold text-white" onClick={() => cancel(false)}>
             Cancelar
           </button>
-          <button className="w-full max-w-xs bg-blue-400 hover:bg-blue-500 py-2 px-4 rounded-md font-bold text-white">
+          <button className="w-full max-w-xs bg-blue-400 hover:bg-blue-500 py-2 px-4 rounded-md font-bold text-white" onClick={registerOrder}>
             Aceptar
           </button>
         </div>
