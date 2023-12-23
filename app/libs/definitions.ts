7e73002a034e3926155a1eb3b68f7858c275f21c
MesachVenegas <mesach.venegas@hotmail.com>
2023-12-23 11:00:38 -0800

    add types to chart and trasactions props

--- app/libs/definitions.ts
+++ app/libs/definitions.ts
@@ -139,3 +139,35 @@ export type OrderSaveProps = {
   volumetric: number;
   subtotal: number;
 }
+
+
+export type ChartProps = {
+  month: string;
+  year: number;
+  clientes: number;
+  }
+
+export type TransactionProps = {
+  id: string;
+  client_id: string,
+  seller_id: string,
+  addressed_name: string,
+  addressed_phone: string,
+  addressed_street: string,
+  addressed_number: string,
+  addressed_colony: string,
+  addressed_city: string,
+  addressed_zip: string,
+  addressed_state: string,
+  addressed_reference: string,
+  package_length: number,
+  package_width: number,
+  package_height: number,
+  package_weight: number,
+  total: number,
+  type: string,
+  payment_type: string,
+  registerAt: Date,
+  updateAt: Date,
+  client: ClientProps,
+}
