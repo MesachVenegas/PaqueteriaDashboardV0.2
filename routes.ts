78f4e34642a039fb91b4ae975c2c81f4577c9a11
MesachVengas <mesach.venegas@hotmail.com>
2024-02-22 20:03:38 -0800

    create routes

---
+++ routes.ts
@@ -0,0 +1,16 @@
+export const publicRoutes = [
+  '/',
+];
+
+export const authRoutes = [
+  '/login',
+  '/error',
+  '/forgot-password',
+  '/reset-password',
+];
+
+export const apiAuthPrefix = '/api/auth';
+
+
+export const DEFAULT_LOGIN_REDIRECT_URL = '/';
+export const DOMAIN_LOGIN_REDIRECT_URL = '/';
