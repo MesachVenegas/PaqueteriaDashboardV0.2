8e38b6f52de79e610d93b95f340b589be2afefae
MesachVengas <mesach.venegas@hotmail.com>
2024-02-22 20:01:44 -0800

    changes database estructure

--- prisma/migrations/20231230224838_fix_coin/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `coin15` on the `Order` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `Order` DROP COLUMN `coin15`,
-    ADD COLUMN `coin5` DOUBLE NOT NULL DEFAULT 0;
