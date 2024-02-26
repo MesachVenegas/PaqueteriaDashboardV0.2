b8f5eed1953bcdc0a57204d57ed4ea26cfff6e65
MesachVengas <mesach.venegas@hotmail.com>
2024-02-25 22:55:02 -0800

    create gate to handle user role access

---
+++ components/role-gate.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import { redirect } from "next/navigation";
+
+interface RoleGateProps {
+  children: React.ReactNode;
+  allowedRole: 'user' | 'admin';
+  role: string;
+}
+
+const RoleGate = ({ children, allowedRole, role }: RoleGateProps) => {
+  if(role !== allowedRole){
+    redirect('/error')
+  }
+
+  return <>{children}</>;
+}
+
+export default RoleGate;
