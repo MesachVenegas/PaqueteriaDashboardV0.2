d756ca02e1490e58537691026542d449e366c1d5
MesachVengas <mesach.venegas@hotmail.com>
2024-02-25 22:50:52 -0800

    change sale point components

--- components/dashboard/sales/clientSale/ClientSalePoint.tsx
+++ components/dashboard/sales/clientSale/ClientSalePoint.tsx
@@ -1,28 +1,12 @@
-'use client'
+import { getAllProducts } from "@/data/products";
+import SaleForm from "@/components/sale-addresser-form";
 
-import { useState } from "react";
-import { ClientProps, ProductProps } from "@/app/libs/definitions";
-import ClientSaleTable from "../tables/ClientSaleTable";
-import Pagination from "../../pagination/Pagination";
-import SaleForm from "../form/SaleForm";
 
-export default function ClientSalePoint({ clients, count, prices} : { clients: ClientProps[], count: number, prices: ProductProps[]} ) {
-  const [clientSelected, setClientSelected] = useState<ClientProps>()
 
+export default async function ClientSalePoint() {
+  const products= await getAllProducts();
 
   return (
-    <>
-      {
-        clientSelected ? null : (
-          <div className="w-full xl:w-96">
-            <ClientSaleTable  data={clients} selectClient={setClientSelected}/>
-            <Pagination count={count} />
-          </div>
-        )
-      }
-      <div className="w-full">
-        <SaleForm setClient={setClientSelected} client={clientSelected as ClientProps} prices={prices} />
-      </div>
-    </>
+      <SaleForm products={products} />
   )
 }
