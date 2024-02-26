72cfc681f35fda5b802a38899f762a3a913fbcdb
MesachVengas <mesach.venegas@hotmail.com>
2024-02-25 22:55:23 -0800

    change input path importation

--- components/register-user.tsx
+++ components/register-user.tsx
@@ -17,7 +17,7 @@ import {
   FormLabel,
   FormMessage
 } from '@/components/ui/form';
-import { Input } from '@/components/ui/Input';
+import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { registerNewUser } from '@/actions/user';
 import { TabsContent } from '@/components/ui/tabs';
