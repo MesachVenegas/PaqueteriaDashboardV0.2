1e945788c04c270367c6cec1e748d0da1a47b341
MesachVengas <mesach.venegas@hotmail.com>
2024-02-25 22:48:41 -0800

    create utils to shadcn-ui

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,5 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const valueFormatter = (number: number) => `$ ${new Intl.NumberFormat("mx", { style: 'decimal', minimumFractionDigits: 2, maximumFractionDigits: 2 }).format(number)}`
