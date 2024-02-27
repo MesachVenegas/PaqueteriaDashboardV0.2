c9a86cad2b4946e50f46cab9847760e2092ea68a
MesachVengas <mesach.venegas@hotmail.com>
2024-02-27 14:19:30 -0800

    change components and add client method

--- app/dashboard/sales/page.tsx
+++ app/dashboard/sales/page.tsx
@@ -1,10 +1,12 @@
 import { Metadata } from 'next'
-import { getClients, getProducts } from '@/app/libs/data';
-import { ClientProps } from '@/app/libs/definitions';
-import Button from '@/components/dashboard/button/Button';
-import ClientSalePoint from '@/components/dashboard/sales/clientSale/ClientSalePoint';
+
+
+import AddClientButton from '@/components/add-client-btn';
+import ClientTableToSale from '@/components/client-sale-table';
 import SearchBar from '@/components/dashboard/search/SearchBar'
-import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
+import { getClientBySearch, getClientCount } from '@/data/clients';
+import SaleForm from '@/components/sale-addresser-form';
+import { getAllProducts } from '@/data/products';
 
 export const metadata: Metadata = {
   title: 'Ventas | Paqueteria 5 Estrellas',
@@ -14,20 +16,21 @@ export const metadata: Metadata = {
 export default async function Sales({ searchParams }: { searchParams: { search: string, page: string }}) {
   const search = searchParams?.search || '';
   const page = searchParams?.page || '1';
-  const response = await getClients(search, parseInt(page));
-  const { count, clients } = Array.isArray(response) ? { count: 0, clients: [] } : response;
-  const responseProducts = await getProducts(parseInt(page), search);
-  const { products, countProducts } = Array.isArray(responseProducts) ? { countProducts: 0, products: [] } : responseProducts;
+  const clients = await getClientBySearch(search, parseInt(page));
+  const clientCount = await getClientCount();
+  const products= await getAllProducts();
+
 
 
   return (
     <div className='flex flex-col gap-4  p-6'>
       <div className='flex w-full justify-between'>
         <SearchBar placeholder='Buscar un Cliente' />
-        <Button source='/dashboard/clientes/create' text='Agregar Cliente' icon={faUserPlus} type='link' />
+        <AddClientButton />
       </div>
-      <div className='flex flex-col xl:flex-row gap-4'>
-        <ClientSalePoint clients={clients as ClientProps[]} count={count} prices={products} />
+      <div className='flex flex-col 2xl:flex-row gap-4'>
+        <ClientTableToSale  clients={clients} count={clientCount}/>
+        <SaleForm  products={products}/>
       </div>
     </div>
   )
