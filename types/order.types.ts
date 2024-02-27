ac441da82a122bcc371ba4c0089463536a64b6b4
MesachVengas <mesach.venegas@hotmail.com>
2024-02-27 13:48:33 -0800

    add order checkout properties

--- types/order.types.ts
+++ types/order.types.ts
@@ -1,5 +1,27 @@
+import { ClientProps } from "./client.types";
 import { ProductProps } from "./product.types";
+import { UserProps } from "./user.types";
 
 export interface SaleFormProps {
   products: ProductProps[]
+}
+
+export interface OrderCheckOutProps {
+  client: ClientProps | null,
+  seller: UserProps | null,
+  addresser_name: string,
+  street: string,
+  number: string,
+  colony: string,
+  delegation: string,
+  zip_code: string,
+  state: string,
+  references: string,
+  pkg_length: string,
+  pkg_height: string,
+  pkg_width: string,
+  pkg_weight: string,
+  delivery_type: 'land' | 'air',
+  total: string,
+  product_selected: string
 }
