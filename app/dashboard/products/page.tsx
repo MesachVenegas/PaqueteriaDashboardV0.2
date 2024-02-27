75dda7f7676a7da3fe54f807b68f40e3ea2a425c
MesachVengas <mesach.venegas@hotmail.com>
2024-02-27 13:53:38 -0800

    changes fetching data an to way to add new product component

--- app/dashboard/products/page.tsx
+++ app/dashboard/products/page.tsx
@@ -1,12 +1,14 @@
-import { deleteProduct } from '@/app/libs/actions'
-import { getProducts } from '@/app/libs/data'
-import { ProductProps } from '@/app/libs/definitions'
-import Button from '@/components/dashboard/button/Button'
-import Pagination from '@/components/dashboard/pagination/Pagination'
-import SearchBar from '@/components/dashboard/search/SearchBar'
-import ProductsTable from '@/components/dashboard/tables/ProductsTable'
-import { faBoxesPacking } from '@fortawesome/free-solid-svg-icons'
 import { Metadata } from 'next'
+
+import { faBoxesPacking } from '@fortawesome/free-solid-svg-icons'
+
+import { ProductProps } from '@/app/libs/definitions'
+import { getProductsBySearch, getProductsCount } from '@/data/products'
+import Button from '@/components/dashboard/button/Button'
+import SearchBar from '@/components/dashboard/search/SearchBar'
+import Pagination from '@/components/Pagination'
+import ProductsTable from '@/components/dashboard/tables/ProductsTable'
+import AddProductButton from '@/components/add-product-btn'
 
 export const metadata: Metadata = {
   title: 'Productos | Paqueteria 5 Estrellas',
@@ -17,24 +19,18 @@ export const metadata: Metadata = {
 export default async function Products({ searchParams }: { searchParams: { search: string, page: string }}) {
   const search = searchParams?.search || '';
   const page = searchParams?.page || '1';
-  const response = await getProducts(parseInt(page), search);
-  const { countProducts, products } = Array.isArray(response) ? { countProducts: 0, products: [] } : response;
-
-  const delProduct = async (id:number) => {
-    await deleteProduct(id)
-      .catch((err) => console.log(err))
-  }
-
+  const products = await getProductsBySearch(parseInt(page), search);
+  const count = await getProductsCount();
 
   return (
     <div className="flex flex-col gap-6 p-5">
       <div className="flex w-full justify-between">
         <SearchBar placeholder="Buscar un producto..." />
-        <Button text="Agregar producto" icon={faBoxesPacking}  type='link' source='/dashboard/productos/add'/>
+        <AddProductButton />
       </div>
       <div className="flex flex-col">
-        <ProductsTable products={products as ProductProps[]} deleteProd={deleteProduct} />
-        <Pagination count={countProducts}  />
+        <ProductsTable products={products as ProductProps[]} count={count}/>
+        <Pagination count={count}  />
       </div>
     </div>
   )
